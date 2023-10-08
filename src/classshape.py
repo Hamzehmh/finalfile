e01e887a618962c76b8cccf03bebfa9677b458c6
hamzehmhe <hamzeh.mh61@gmaile.com>
2023-10-08 12:15:20 +0330

    fot 16/07/1402

--- src/classshape.py
+++ src/classshape.py
@@ -2,8 +2,8 @@ class Person:
 
      def __init__(self, f_name, l_name, age):
         self.f_name = f_name
-        self.f_name = l_name
+        self.l_name = l_name
         self.age = age
 
      def info(self):
-        print("My info is : "+self.f_name)
+        print("My info is : "+self.f_name+" "+self.l_name)
--- src/fileshape.py
+++ src/fileshape.py
@@ -2,7 +2,8 @@ from classshape import Person
 
 
 person1 = Person("ALi", "Rahmati", 42)
-#print(person1.l_name)
+
+print(person1.f_name,person1.l_name, person1.age)
 
 person1.info()
 
