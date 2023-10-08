afe9e7a28b1c4165d01cc0b3a3e96c7fdef84f7c
Hamzehmhe <hamzeh.mh61@gmail.com>
2023-10-08 19:19:11 +0330

    Home 16-07-1402 19:20

--- src/classshape.py
+++ src/classshape.py
@@ -1,16 +1,38 @@
 class Shape:
+   
    def __init__(self, color = "GRAY"):
       self.color = color
 
-
+#------------------------------------------------------------------
 
 class Circle(Shape):
-    def __init__(self, r, color = "RED") :
-        self.color = color
+  """this is a Circle class-is my exersize
+
+  Arguments:
+      Shape --this is my frist test
+  """
+
+  def __init__(self, r, color = "rrr"):
+        super().__init__(color)
         self.r = r
 
-    def area(self,):
+  def area(self,):
       return 3.14 * self.r * self.r   
-
-    def mohit(self,):
+    
+  def mohit(self,):
       return 3.14 * self.r *2 
+#------------------------------------------------------------------------------    
+    
+class Regtangle(Shape):
+    def __init__(self, tol, arz, color="green"):
+        super().__init__(color)
+        self.tol = tol
+        self.arz = arz
+
+    def area(self,):
+      return self.tol * self.arz
+        
+    def mohit(self,):
+      return 2*self.tol + 2* self.arz
+    #---------------------------------------------------------------------------------
+        
--- src/fileshape.py
+++ src/fileshape.py
@@ -1,8 +1,17 @@
+from classshape import Shape
 from classshape import Circle
+from classshape import Regtangle
 
 
-c = Circle(3536)
+c = Circle(5)
 
 print(c.area())
 print(c.mohit())
 print(c.color)
+
+b = Regtangle(2, 4)
+
+
+print(b.area())
+print(b.mohit())
+print(b.color)
