76aa7fba1dfd3c915ecb86b98e79f09132d5d401
hamzehmhe <hamzeh.mh61@gmaile.com>
2023-10-08 12:34:32 +0330

    again fotrosi 16/07/1402

--- src/classshape.py
+++ src/classshape.py
@@ -1,5 +1,5 @@
 class Person:
-
+   
      def __init__(self, f_name, l_name, age):
         self.f_name = f_name
         self.l_name = l_name
@@ -7,3 +7,10 @@ class Person:
 
      def info(self):
         print("My info is : "+self.f_name+" "+self.l_name)
+
+#=====================================================================
+class Student(Person):
+    
+    def info(self):
+        print("my text is:" +self.f_name)
+        
--- src/fileshape.py
+++ src/fileshape.py
@@ -1,9 +1,13 @@
 from classshape import Person
+from classshape import Student
 
 
 person1 = Person("ALi", "Rahmati", 42)
-
 print(person1.f_name,person1.l_name, person1.age)
-
 person1.info()
+#=================================================
+
+person2 = Student("Hasan", "gholami", 22)
+print(person2.f_name, person2.l_name, person2.age)
+person2.info()
 
