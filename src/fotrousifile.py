656a35817cb05fe84ad082d0a6431533d4d230ab
Hamzehmhe <hamzeh.mh61@gmail.com>
2023-10-10 19:53:45 +0330

    from my swity Home 18-07-1402 19:54

--- src/fotrousifile.py
+++ src/fotrousifile.py
@@ -17,4 +17,4 @@ per3 = Karmand("\nhasan", "kashani", 3923, 543,"khadamat")
 print(per3.f_name, per3.l_name, per3.ID, per3.age)
 per3.info()
 per3.taskk()
-#3#
+###va ali dare pish mire in python programing
