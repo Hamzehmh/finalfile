6fef7db6a1e83e9a1a3ce06336eb800b9cf3ee1a
hamzehmhe <hamzeh.mh61@gmaile.com>
2023-10-15 17:02:16 +0330

    fotrousiiiii 23/07/1402 17:05 Dige saat kari dare tamom mishe

--- src/test.py
+++ src/test.py
@@ -1,7 +1,7 @@
 import random
-tas1=random.randint(200,8000)
-tas2=random.randint(1007666,7889917)
-tas3=random.randint(54335,1487665)
+tas1=random.randint(1,6)
+tas2=random.randint(1,6)
+tas3=random.randint(1,6)
 print(tas1)
 print(tas2)
 print(tas3)
@@ -13,3 +13,20 @@ def test(a , b , c):
 
 x = test(tas1 , tas2 , tas3)
 print(x)
+
+class Joftshish:
+    def __init__(self,tas11, tas22):
+        self.tas11 = tas11
+        self.tas22 = tas22
+
+    def jofttshish(self):
+        print("tashaye man az in gharar ast"+ "-----"+ str(self.tas11)+" AND "+ str(self.tas22))
+    def zarbjoftshish(self):
+        return tas1 * tas2
+        
+
+x_1 = Joftshish(tas1 , tas2)        
+x_1.jofttshish()
+x_1.zarbjoftshish()
+
+
