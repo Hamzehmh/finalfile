603c2bd7d6e345e72ee84ab27a1ec7217e147e22
hamzehmhe <hamzeh.mh61@gmaile.com>
2023-10-30 16:39:40 +0330

    fot 08-08-1402 16:40

--- src/testt.py
+++ src/testt.py
@@ -6,8 +6,19 @@ v = u**2
 
 
 print(x)
-print(y)
-print(z)
-print(u)
-print(v)
-print(x+y)
+
+my_list = [1, 2, 5, 8 ,11]
+print(my_list)
+print(type(my_list))
+
+my_dict = {1, 6, 8, 99}
+print(my_dict)
+print(type(my_dict))
+
+my_tuple = (1,2,3,)
+print(my_tuple)
+print(type(my_tuple))
+
+my_set =  {"a":1 , "b":2, "c":3}
+print(my_set)
+print(type(my_set))
