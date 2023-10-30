ff34cb8e0537cb03ffc0acbbfcf24785d636f362
hamzehmhe <hamzeh.mh61@gmaile.com>
2023-10-30 16:42:43 +0330

    fot 08-08-1402 16:45

--- src/testt.py
+++ src/testt.py
@@ -11,14 +11,14 @@ my_list = [1, 2, 5, 8 ,11]
 print(my_list)
 print(type(my_list))
 
-my_dict = {1, 6, 8, 99}
-print(my_dict)
-print(type(my_dict))
+my_set = {1, 6, 8, 99}
+print(my_set)
+print(type(my_set))
 
 my_tuple = (1,2,3,)
 print(my_tuple)
 print(type(my_tuple))
 
-my_set =  {"a":1 , "b":2, "c":3}
-print(my_set)
-print(type(my_set))
+my_dict =  {"a":1 , "b":2, "c":3}
+print(my_dict)
+print(type(my_dict))
