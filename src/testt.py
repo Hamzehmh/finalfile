57aa2d316bce26c17c6ad2ecdadde499ee6b66bc
hamzehmhe <hamzeh.mh61@gmaile.com>
2023-11-06 15:37:49 +0330

    FOT 15-08-1402 15:38

--- src/testt.py
+++ src/testt.py
@@ -6,21 +6,22 @@ v = u**2
 
 
 print("my print is : " + str(x)  )
-
+print("\n-----------------MY LIST-------------------\n")
 my_list = [1, 2, 5, 8 ,11]
 print("\n my LIST is : " +str( my_list ))
 print(type(my_list))
-
+print("\n-----------------MY SET--------------------\n")
 my_set = {1, 6, 8, 99}
 print("\n my SET is : " +str( my_set ))
 print(type(my_set))
-
+print("\n-----------------MY TUPLE-------------------\n")
 my_tuple = (1,2,3,)
-print("\n my TUPLE is : " +str( my_tuple ))
+print(" my TUPLE is : " +str( my_tuple ))
 print(type(my_tuple))
-
+print("\n-----------------MY DICT-------------------\n")
 my_dict =  {"a":1 , "b":2, "c":3}
 print("\n my DICT is : " +str( my_dict ))
 print(type(my_dict))
 
+print("\n-----------------END-------------------")
 # II so happpy for python learn-this is a big Question-98566
