ca137ec40a981c9543fe1256167088eb36fdfeb3
hamzehmhe <hamzeh.mh61@gmaile.com>
2023-11-29 07:34:35 +0330

    fotrousiclass 08-09-1402 07:30

--- src/fotrousiclass.py
+++ src/fotrousiclass.py
@@ -15,7 +15,7 @@ class Person:
 
     def info(self):
         print("my info is :" +self.f_name+" "+self.l_name+ " "+str(self.age)+ " " +str(self.ID))
-  #---------------------------------------------------------------------
+  #-----------------------------------------------------------------------
 class Manager(Person):
      def __init__(self,f_name,l_name,age,ID, semat):
         super().__init__(f_name,l_name,age,ID)
@@ -33,7 +33,7 @@ class Karmand(Person):
      def taskk(self):
           print("my task in fotrousi company is :"+self.task)
 
-#-----------------------------------------------------------------------
+#-------------------------------------------------------------------------
 class Moshtari(Manager):
      def __init__(self, f_name, l_name, age, ID, semat, kharid):
         super().__init__(f_name, l_name, age, ID,semat)
