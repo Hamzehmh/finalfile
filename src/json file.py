4bb0004b86391f47466645ed2d44cee4112ad9e6
hamzehmhe <hamzeh.mh61@gmaile.com>
2023-12-02 11:12:27 +0330

    fot 11-08-1402 11:15

--- src/json file.py
+++ src/json file.py
@@ -1,42 +1,11 @@
 import json
 
-#print("----------------START------------------")
-#my_info = [
-  {
-    "fa": "Man dar hal e hazer inja kar mikonam",
-    "en": "I'm working at the moment",
-    "type": "positive",
-    "tense": "Present Continuous"
-  }]
-print("----------------DUMP---------------------")
-ali = json.dumps(my_info)
-print(ali)
-print("----------------LOAD---------------------")
-hasam = json.loads(ali)
-print("===>",hasam)
-print("----------------END----------------------")
+file = "data.json"
 
-
-
-#json.dump(my_info,fp=["datata.json"])
-
-with open("data_file.json", "w") as f:
-    json.dump(data, f)
-
-
-
-import json
- 
-f = open('../src/data.json')
- 
-# returns JSON object as 
-# a dictionary
-data = json.load(f)
- 
-# Iterating through the json
-# list
-for i in data['emp_details']:
-    print(i)
- 
-# Closing file
-f.close()
+with open(file, "r") as json_file:
+    data = json.load(json_file)
+    name_data = (data["tense"])
+    for i in name_data:
+        en = (i[en])
+        age = (i[type])
+        print(f'(en) is (type)')
