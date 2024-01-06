981e66de7f2b622f7e2f7183e2d5f1a1e1284082
hamzehmhe <hamzeh.mh61@gmaile.com>
2024-01-06 10:40:25 +0330

    fot 16/10/1402 10:40

--- src/review.py
+++ src/review.py
@@ -4,3 +4,13 @@ my_list = ["kamal", "jamal", "mamal", 2, 86]
 my_list.append("dess")
 
 print(my_list[::-1])
+
+
+mytuple = (1, 2, 3)
+print(mytuple.count(2))
+print(mytuple)
+
+
+mydict = {"a":1, "b": 2, "c" : 3}
+print(mydict)
+print(mydict["b"])
