e67da0c574d225e564f0f3ed676b395252d7088b
hamzehmhe <hamzeh.mh61@gmaile.com>
2024-01-07 15:10:23 +0330

    fot 17/10/1402 15:10

--- src/review.py
+++ src/review.py
@@ -3,14 +3,15 @@ my_list = ["kamal", "jamal", "mamal", 2, 86]
 #print(my_list)=
 my_list.append("dess")
 
-print(my_list[::-1])
+print(my_list[:])
 
 
-mytuple = (1, 2, 3)
+mytuple = (1, 5, 3)
 print(mytuple.count(2))
 print(mytuple)
 
 
 mydict = {"a":1, "b": 2, "c" : 3}
+
 print(mydict)
 print(mydict["b"])
